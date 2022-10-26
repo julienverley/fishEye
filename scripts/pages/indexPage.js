bc9635f402a1d1a10f08308f1e8c0316fa35ddc2
Julien Verley <mjules@gmail.com>
2022-10-26 10:01:21 +0200

    fetch data remove ./

--- scripts/pages/indexPage.js
+++ scripts/pages/indexPage.js
@@ -1,28 +1,28 @@
-// Index page 
+// Index page
 
 // Get photographers datas from .json
-function init() { 
-  fetch('./data/photographers.json') // Get .json
+function init() {
+  fetch("data/photographers.json") // Get .json
     .then((response) => response.json()) // Make JS object
     .then((data) => {
       const { photographers } = data;
-      displayData(photographers); // Launch the display 
+      displayData(photographers); // Launch the display
     });
 }
 init();
 
 // Add userCardDOM nodes
-function displayData(photographers) { 
-  const photographersSection = document.querySelector('.photographers-section'); 
-  
+function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographers-section");
+
   // For each photographer
   photographers.forEach((photographer) => {
     const photographerModel = photographerFactory(photographer); // Get data and build DOM
     const userCardDOM = photographerModel.getPhotographerCardDOM(); // Build DOM
     photographersSection.appendChild(userCardDOM); // Create node userCardDOM
 
-    // On click event, URL  
-    userCardDOM.addEventListener('click', () => { 
+    // On click event, URL
+    userCardDOM.addEventListener("click", () => {
       window.location.href = `photographer.html?id=${photographer.id}`;
     });
   });
